fa41431d4c6cf211d221a85ead3b2352287e7ec0
Edward Xia <xias1@uci.edu>
2017-03-09 00:29:20 -0800

    Add listRooms

--- src/main/java/edu/ics/game/client/Client.java
+++ src/main/java/edu/ics/game/client/Client.java
@@ -2,6 +2,8 @@ package edu.ics.game.client;
 
 import java.net.URISyntaxException;
 
+import org.json.JSONObject;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
@@ -20,6 +22,8 @@ public class Client {
 			public void call(Object... args) {
 				socket.emit("setName", "Test");
 				socket.emit("joinRoom", "room1");
+				socket.emit("listRooms");
+
 //				socket.emit("joinRoom", "room2");
 //				socket.emit("leaveRoom", "room2");
 //				socket.disconnect();
@@ -27,6 +31,11 @@ public class Client {
 		}).on("message", new Emitter.Listener() {
 			public void call(Object... args) {
 				System.out.println((String)args[0]);
+			}
+		}).on("rooms", new Emitter.Listener() {
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				System.out.println(data.toString());
 			}
 		}).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
 			public void call(Object... args) {}
