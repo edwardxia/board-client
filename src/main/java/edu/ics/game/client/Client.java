2bf598c2ed3265b276c93723ec5c9181b201063c
Edward Xia <xias1@uci.edu>
2017-03-08 23:44:13 -0800

    Print message from server

--- src/main/java/edu/ics/game/client/Client.java
+++ src/main/java/edu/ics/game/client/Client.java
@@ -18,16 +18,15 @@ public class Client {
 		
 		socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
 			public void call(Object... args) {
-				socket.emit("setName", "Test user");
-				socket.emit("joinRoom", "my room");
-				socket.emit("joinRoom", "another room");
-				socket.emit("leaveRoom", "another room");
-
-				// socket.disconnect();
+				socket.emit("setName", "Test");
+				socket.emit("joinRoom", "room1");
+//				socket.emit("joinRoom", "room2");
+//				socket.emit("leaveRoom", "room2");
+//				socket.disconnect();
 			}
 		}).on("message", new Emitter.Listener() {
 			public void call(Object... args) {
-				System.out.println("message received");
+				System.out.println((String)args[0]);
 			}
 		}).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
 			public void call(Object... args) {}
