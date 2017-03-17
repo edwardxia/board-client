f3a8df3444510bd7a35fb0fb544364ea648038aa
Edward Xia <xias1@uci.edu>
2017-03-16 22:04:26 -0700

    Update checkers

--- src/main/java/edu/ics/game/client/Checkers.java
+++ src/main/java/edu/ics/game/client/Checkers.java
@@ -1,32 +1,48 @@
 package edu.ics.game.client;
 
-import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBoundsType;
 
 public class Checkers extends GamePieceFactory{
 	public Node createPiece(int piece, int column, int row) {
-		Group group = new Group();
+		StackPane pane = new StackPane();
 		Rectangle background = new Rectangle(0, 0, 60, 60);
 		background.setFill(((column + row) % 2) == 0 ? Color.WHITE : Color.GRAY);
-		group.getChildren().add(background);
-		
-		switch (piece) {
-		case 0:
-		case 2:
-			Circle white = new Circle(30, 30, 25);
-			white.setFill(Color.WHITE);
-			group.getChildren().add(white);
-			break;
-		case 1:
-		case 3:
-			Circle black = new Circle(30, 30, 25);
-			black.setFill(Color.BLACK);
-			group.getChildren().add(black);
-			break;
+		pane.getChildren().add(background);
+
+		if (piece >=0 && piece < 8) {
+			Circle circle = new Circle(30, 30, 25);
+			circle.setStrokeWidth(0);
+
+			if (piece / 4 == 0) {
+				circle.setFill(Color.WHITE);
+				circle.setStroke(Color.BLACK);
+			} else {
+				circle.setFill(Color.BLACK);
+				circle.setStroke(Color.WHITE);
+			}
+
+			if (piece % 2 == 1) {
+				circle.setStrokeWidth(3);
+			}
+
+			pane.getChildren().add(circle);
+			
+			if ((piece % 4) / 2 == 1) {
+				Text text = new Text("♔");
+				text.setFont(new Font(32));
+				text.setFill((piece / 4 == 0) ? Color.BLACK : Color.WHITE);
+				text.setBoundsType(TextBoundsType.VISUAL);
+				pane.getChildren().add(text);
+			}
 		}
-		return group;
+
+		return pane;
 	}
 }
