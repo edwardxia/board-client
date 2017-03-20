0f1c2ccb296a6db2f59eb4d35bcff7d2e30009a8
andrewle95 <leaa1@uci.edu>
2017-03-19 17:10:31 -0700

    Added Winner pop up

--- src/main/java/edu/ics/game/client/GameRoomController.java
+++ src/main/java/edu/ics/game/client/GameRoomController.java
@@ -14,7 +14,10 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
@@ -23,6 +26,9 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class GameRoomController extends Controller {
 	private Game game = null;
@@ -143,7 +149,6 @@ public class GameRoomController extends Controller {
 					initializeGrid(columns, rows);
 					initializeGame(jsonGame.getString("name"));
 					JSONArray jsonBoard = jsonGame.getJSONArray("board");
-					System.out.println(jsonGame);
 					for (int column = 0; column < columns; column++) {
 
 						JSONArray jsonRow = jsonBoard.getJSONArray(column);
@@ -181,6 +186,18 @@ public class GameRoomController extends Controller {
 					if (data.has("playerIndex")) {
 						playerIndex = data.getInt("playerIndex");
 					}
+					String message;
+					if(jsonGame.getBoolean("ended")){
+						if (jsonGame.getInt("winner") == 0){
+							message = "Winner is " + data.getJSONArray("players").getJSONObject(0).getString("name");
+						}else if (jsonGame.getInt("winner") == 1){
+							message = "Winner is " + data.getJSONArray("players").getJSONObject(1).getString("name");
+						}else{
+							message = "No Winner / Tie";
+						}
+						AlertBox.display("Game Over", message);
+					}
+					
 				} catch (JSONException e) {
 					e.printStackTrace();
 				}
@@ -211,5 +228,30 @@ public class GameRoomController extends Controller {
 		}catch (JSONException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static class AlertBox{
+		
+		public static void display(String title, String message){
+			Stage window = new Stage();
+			
+			window.initModality(Modality.APPLICATION_MODAL);
+			window.setTitle(title);
+			window.setMinWidth(500);
+			
+			Label label = new Label();
+			label.setText(message);
+			Button closeButton = new Button("Ok");
+			closeButton.setOnAction(e -> window.close());
+			
+			VBox layout = new VBox(10);
+			layout.getChildren().addAll(label, closeButton);
+			layout.setAlignment(Pos.CENTER);
+			
+			Scene scene = new Scene(layout);
+			window.setScene(scene);
+			window.showAndWait();
+		}
+		
 	}
 }
